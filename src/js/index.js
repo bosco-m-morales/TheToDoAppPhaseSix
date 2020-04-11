ed24a5dd0bb889472fd34b739101c31ef5889681
Bosco Moises Morales <bmoises.morales@gmail.com>
2020-04-11 16:29:03 +0000

    Push One

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,16 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
-
 //render your react application
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>It's Bosco, Bitch!</h1>
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(<Home />, document.querySelector("#app"));
