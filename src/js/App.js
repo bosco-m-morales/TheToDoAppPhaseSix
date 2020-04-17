40b57c77c5a2ce972bf80a9b6ea90141043530c9
Bosco Moises Morales <bmoises.morales@gmail.com>
2020-04-17 13:08:14 +0000

    update 1

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import { ToDoItem } from "./component/ToDoItem";
 import { toDosData } from "./data/ToDosData";
-/*
-export function App() {
-	const todoComponents = toDosData.map(todos => (
-		<ToDoItem key={todos.id} todos={todos} />
-	));
-	return <div className="todo-list">{todoComponents}</div>;
-}
-*/
+
 
 export class App extends Component {
 	constructor() {
